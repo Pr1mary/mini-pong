4c7e71ccd0bff181995afee49a081332a284e002
Muhammad Rafli Satriandi <raflisatriandi@gmail.com>
2020-08-06 13:37:18 +0700

    all player added
    + player 1 and player 2 added
    + player boundaries area bug fixed
    + ball texture added

--- mini-pong/Game1.cs
+++ mini-pong/Game1.cs
@@ -12,6 +12,9 @@ namespace mini_pong
         Texture2D player1;
         Vector2 playerPos1;
 
+        Texture2D player2;
+        Vector2 playerPos2;
+
         float playerSpeed;
 
         GraphicsDeviceManager graphics;
@@ -33,6 +36,7 @@ namespace mini_pong
         {
             // TODO: Add your initialization logic here
             playerPos1 = new Vector2(graphics.PreferredBackBufferWidth / 8, graphics.PreferredBackBufferHeight / 2);
+            playerPos2 = new Vector2(7*(graphics.PreferredBackBufferWidth / 8), graphics.PreferredBackBufferHeight / 2);
             playerSpeed = 500f;
 
             base.Initialize();
@@ -48,7 +52,8 @@ namespace mini_pong
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            player1 = Content.Load<Texture2D>("pong player");
+            player1 = Content.Load<Texture2D>("player");
+            player2 = Content.Load<Texture2D>("player");
         }
 
         /// <summary>
@@ -77,16 +82,20 @@ namespace mini_pong
             if (kstate.IsKeyDown(Keys.W)) playerPos1.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (kstate.IsKeyDown(Keys.S)) playerPos1.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             //player 2 control
-            if (kstate.IsKeyDown(Keys.W)) playerPos1.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (kstate.IsKeyDown(Keys.S)) playerPos1.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Up)) playerPos2.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Down)) playerPos2.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             //player area collision
             //player 1 bounds
-            //if (playerPos1.Y > graphics.PreferredBackBufferHeight - player1.Height / 2)
-            //    playerPos1.Y = graphics.PreferredBackBufferHeight - player1.Height / 2;
-            //if (playerPos1.Y < player1.Height / 2)
-            //    playerPos1.Y = player1.Height / 2;
+            if (playerPos1.Y > graphics.PreferredBackBufferHeight - player1.Height / 2)
+                playerPos1.Y = graphics.PreferredBackBufferHeight - player1.Height / 2;
+            if (playerPos1.Y < player1.Height / 2)
+                playerPos1.Y = player1.Height / 2;
             //player 2 bounds
+            if (playerPos2.Y > graphics.PreferredBackBufferHeight - player2.Height / 2)
+                playerPos2.Y = graphics.PreferredBackBufferHeight - player2.Height / 2;
+            if (playerPos2.Y < player2.Height / 2)
+                playerPos2.Y = player2.Height / 2;
 
             base.Update(gameTime);
         }
@@ -101,7 +110,8 @@ namespace mini_pong
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(player1, playerPos1, null, Color.White, 0f, new Vector2(player1.Width/2, player1.Height/2), .1f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(player1, playerPos1, null, Color.White, 0f, new Vector2(player1.Width/2, player1.Height/2), 1f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(player2, playerPos2, null, Color.White, 0f, new Vector2(player2.Width / 2, player2.Height / 2), 1f, SpriteEffects.None, 0f);
             spriteBatch.End();
 
             base.Draw(gameTime);
